3c47a8158f4aaae3e601706270890094ce8650a2
Sara Kane <kane.sarak@gmail.com>
2020-06-08 14:10:34 -0700

    Refactor event listeners into a loop
    Co-authored-by: Ian Scott <chesnekov@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,31 @@
 $(document).ready(function() {  
-  $("h1").click(function() {
-    alert("This is a header.");
-    alert("I told you, THIS IS A HEADER!");
+  // $("h1").click(function() {
+  //   alert("This is a header.");
+  //   alert("I told you, THIS IS A HEADER!");
+  // });
+
+  // $("p").click(function() {
+  //   alert("This is a paragraph.");
+  // });
+
+  // $("img").click(function() {
+  //   alert("This is an image.");
+  // });
+
+  let eventListeners = ["h1", "p", "img"];
+  eventListeners.forEach(function(listener) {
+    $(listener).click(function() {
+      let myElement;
+      if (listener === "h1") {
+        myElement = "header";
+      } else if (listener === "p") {
+        myElement = "paragraph";
+      }
+      else if (listener === "img") {
+        myElement = "image";
+      }
+      alert("This is a " + myElement);
+    });
   });
 
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
-  });
 });
